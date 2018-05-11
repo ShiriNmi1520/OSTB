c7818dfbe946cf61b0f16b07592e988aaebdec9d
ShiriNmi1520 <s20026352@gmail.com>
2018-05-11 16:55:35 +0800

    修改emit內容

--- Server.js
+++ Server.js
@@ -5,7 +5,7 @@ http.listen(process.env.PORT || 48763, function () {
 io.on('connection', function (socket) {
     socket.on('test', function (data) {
         console.log(data);
-        io.emit('test', "Success");
+        io.emit('test', "success " + data);
     });
     socket.on('disconnect', function () {
         console.log('say goodbye');
