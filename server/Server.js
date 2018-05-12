d7809758b854771673b71ebfcd52b6e9ffb6cd6e
ShiriNmi1520 <s20026352@gmail.com>
2018-05-12 17:34:15 +0800

    Test firebase

--- server/Server.js
+++ server/Server.js
@@ -1,4 +1,12 @@
-var express = require('express'), app = express(), http = require('http').Server(app), io = require('socket.io')(http);
+var express = require('express'), app = express(), http = require('http').Server(app), io = require('socket.io')(http), firebase = require('firebase'), firebase_config = {
+    apiKey: "AIzaSyC6V5XWXQCC_zdGWsXPND4OVpwYGS7VsAE",
+    authDomain: "buyao-70f4a.firebaseapp.com",
+    databaseURL: "https://buyao-70f4a.firebaseio.com",
+    projectId: "buyao-70f4a",
+    storageBucket: "buyao-70f4a.appspot.com",
+    messagingSenderId: "409751210552"
+};
+firebase.initializeApp(firebase_config);
 http.listen(process.env.PORT || 48763, function () {
     console.log('Computer listening on :' + process.env.PORT);
 });
@@ -10,6 +18,14 @@ io.on('connection', function (socket) {
     socket.on('disconnect', function () {
         console.log('say goodbye');
         io.emit('test', "ru disconnected?");
+    });
+    socket.on('auth', function (data) {
+        console.log('get login data\nstart auth process..');
+    });
+    socket.on('register', function (data) {
+        console.log("we've received register signal\nstart register process...");
+        console.log(data.email, data.password);
+        io.emit('test', "we got it:)");
     });
 });
 //# sourceMappingURL=Server.js.map
--- server/Server.ts
+++ server/Server.ts
@@ -1,19 +1,43 @@
 const express = require('express'),
 	app = express(),
 	http = require('http').Server(app),
-	io = require('socket.io')(http);
+	io = require('socket.io')(http),
+	firebase = require('firebase'),
+	firebase_config = {
+		apiKey: "AIzaSyC6V5XWXQCC_zdGWsXPND4OVpwYGS7VsAE",
+		authDomain: "buyao-70f4a.firebaseapp.com",
+		databaseURL: "https://buyao-70f4a.firebaseio.com",
+		projectId: "buyao-70f4a",
+		storageBucket: "buyao-70f4a.appspot.com",
+		messagingSenderId: "409751210552"
+
+	};
+
+firebase.initializeApp(firebase_config);
 
 http.listen(process.env.PORT || 48763, () => {
 	console.log('Computer listening on :' + process.env.PORT);
 });
 
 io.on('connection', (socket) => {
+
 	socket.on('test', (data) => {
 		console.log(data);
 		io.emit('test', `success ${data.split(' ').reverse()}`)
 	});
+
 	socket.on('disconnect', () => {
 		console.log('say goodbye');
 		io.emit('test', "ru disconnected?")
 	});
+
+	socket.on('auth', (data) => {
+		console.log('get login data\nstart auth process..');
+	});
+
+	socket.on('register', (data) => {
+		console.log("we've received register signal\nstart register process...");
+		console.log(data.email, data.password);
+		io.emit('test', "we got it:)");
+	})
 });
