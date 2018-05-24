d07468dc6faebe280ebdab8ab99c0030bbf388b3
ShiriNmi1520 <s20026352@gmail.com>
2018-05-24 20:31:24 +0800

    'count'

--- server/Server.js
+++ server/Server.js
@@ -92,7 +92,7 @@ io.on('connection', function (socket) {
         //並將Room內在線人數傳回
         socket.join(data);
         io.to(data).emit('Player joined!');
-        console.log("Now we have " + Object.keys(io.sockets.connected).length + " clients in " + data);
+        console.log("Now we have " + io.sockets.adapter.rooms[data].length + " clients in " + data);
         // socket.room = data;
         // if(io.sockets.clients(data) == 4){
         // 	socket.status = 1;
