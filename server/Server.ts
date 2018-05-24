5502664e6837efc953f446ddfca8d1764e0942dc
ShiriNmi1520 <s20026352@gmail.com>
2018-05-24 21:12:18 +0800

    roomkey

--- server/Server.ts
+++ server/Server.ts
@@ -87,13 +87,13 @@ io.on('connection', (socket) => {
 		//創立房間、隨機生成id並加入
 		//加入後將id返回客戶端om
 			let id: string = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-			socket.join(id);
 			io.to(id).emit('createRoom', id);
 			//roomID會被存放在每個unique-id底下
 			//透過key() 來得到
 			//傳送的data作為遊戲室名稱
 			let RoomKey: string = firebase.database().ref('/rooms/').push({id: id, room: data}).key;
 			console.log(`Created room name ${data}`);
+			console.log(RoomKey);
 	});
 
 	socket.on('getRoomId', () => {
