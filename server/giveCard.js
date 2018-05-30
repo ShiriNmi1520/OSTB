5e64bf592446e0d9cdf5489e1be5e50f72638239
ShiriNmi1520 <s20026352@gmail.com>
2018-05-30 22:39:29 +0800

    ts => js

---
+++ server/giveCard.js
@@ -0,0 +1,25 @@
+var cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var types = ["♠", "♥", "♣", "♦"];
+function getRandom(pools, count) {
+    var arr = [];
+    for (var i = 0; i < count; i++) {
+        var pool = pools[Math.floor(Math.random() * pools.length)];
+        arr.indexOf(pool) == -1 ? arr.push(pool) : i--;
+    }
+    return arr;
+}
+function getRandomWithType(count) {
+    var arr = [];
+    for (var i = 0; i < count; i++) {
+        var pool = cards[Math.floor(Math.random() * cards.length)];
+        var type = types[Math.floor(Math.random() * types.length)];
+        var result = pool + type;
+        arr.indexOf(result) == -1 ? arr.push(result) : i--;
+    }
+    return arr;
+}
+module.exports = {
+    getRandom: getRandom,
+    getRandomWithType: getRandomWithType
+};
+//# sourceMappingURL=giveCard.js.map
