cdb6198b387b6c18d7017c36ea6d96db1b940270
ShiriNmi1520 <s20026352@gmail.com>
2018-06-08 16:48:04 +0800

    Move output to src

---
+++ server/src/giveCard.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const types = ["♠", "♥", "♣", "♦"];
+function getRandom(pools, count) {
+    const arr = [];
+    for (let i = 0; i < count; i++) {
+        let pool = pools[Math.floor(Math.random() * pools.length)];
+        arr.indexOf(pool) === -1 ? arr.push(pool) : i--;
+    }
+    return arr;
+}
+exports.getRandom = getRandom;
+function getRandomWithType(count) {
+    const arr = [];
+    for (let i = 0; i < count; i++) {
+        let pool = cards[Math.floor(Math.random() * cards.length)];
+        let type = types[Math.floor(Math.random() * types.length)];
+        let result = pool + type;
+        arr.indexOf(result) === -1 ? arr.push(result) : i--;
+    }
+    return arr;
+}
+exports.getRandomWithType = getRandomWithType;
+//# sourceMappingURL=giveCard.js.map
