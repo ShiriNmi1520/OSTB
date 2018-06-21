c5bc89c1d3539bae7fa7b179503d959c3194f19d
ShiriNmi1520 <s20026352@gmail.com>
2018-06-21 21:45:00 +0800

    nickName

--- server/src/Server.js
+++ server/src/Server.js
@@ -208,8 +208,11 @@ mainSocket.on("connection", (socket) => {
         let room = "";
         const playerPath = firebase.database().ref(`/room/${data.roomId}/player`);
         const roomPath = firebase.database().ref(`/room/${data.roomId}`);
-        const nickNamePath = firebase.database().ref(`/users/${data.userId}/name`);
+        const nickNamePath = firebase.database().ref(`/users/${data.userId}/`);
         let nickName = "";
+        nickNamePath.once("value", (snap) => {
+            nickName = snap.val().name;
+        });
         playerPath.push({ host: false, nickName: nickName, readyStatus: false, uid: data.userId, socketId: data.socketId });
         playerPath.once("value", (snap) => {
             // mainSocket.socket(socket.id).emit(snap.val());
